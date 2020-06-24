a4523e1724b0328e14ec61c26137f640821b68ff
AnnaDearman <59611559+AnnaDearman@users.noreply.github.com>
2020-06-24 18:10:32 +0100

    Commands for making PRS for height, to act as a control phenotype

---
+++ STEP_04_Polygenic_scores/prs_height.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#$ -cwd           # Set the working directory for the job to the current directory
+#$ -pe smp 1      # Request 1 core
+#$ -l h_rt=24:0:0 # Request 24 hour runtime
+#$ -l h_vmem=20G   # Request 20GB RAM
+#$ -j y
+
+echo "***********************************************************************"
+echo "                 Polygenic scoring - height   "
+echo "***********************************************************************"
+
+PRSice_linux \
+ --A1 Allele1 \
+ --A2 Allele2 \
+ --beta \
+ --pvalue p \
+ --stat b \
+ --snp MarkerName \
+ --base ~/gendep/prs/prs_data/height/GIANT_HEIGHT_Wood_et_al_2014_publicrelease_HapMapCeuFreq.txt \
+ --fastscore \
+ --all-score \
+ --no-regress \
+ --print-snp \
+ --bar-levels 0.0001,0.01,0.05,0.1,0.5,1 \
+ --target ~/gendep/post_imp_qc/post_imp_qc_output/gendep_1kg_imp_QCd_popstratout_removed \
+ --out ~/gendep/prs/prs_data/height/gendep_height_PGS
